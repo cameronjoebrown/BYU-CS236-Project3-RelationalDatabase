9592d07cb3eb8267077252f486f858b36679f133
Cameron <cameronjoebrown@gmail.com>
2019-02-21 16:35:35 -0700

    Initial Commit

---
+++ Project3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project3
+//
+//  Created by Cam on 2/21/19.
+//  Copyright © 2019 Cam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
